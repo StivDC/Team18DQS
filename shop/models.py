3aaff480ae82a035f322be703914abf38534dc44
Joshua Marchant <marchantjr@cardiff.ac.uk>
2019-04-01 13:47:15 +0100

    win files

---
+++ shop/models.py
@@ -0,0 +1,67 @@
+
+from datetime import datetime
+from shop import db, login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20),nullable=False)
+    last_name = db.Column(db.String(30),nullable=False)
+    books = db.relationship('Book', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"Author('{self.first_name}', '{self.last_name}')"
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50),nullable=False)
+    description = db.Column(db.String(2000), nullable=False)
+    price = db.Column(db.Numeric(10,2), nullable=False)
+    image_file = db.Column(db.String(30), nullable=False, default='default.jpg')
+    stock_level = db.Column(db.Integer, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
+    user_cart = db.relationship('Cart', backref='book', lazy=True)
+    user_wishlist = db.relationship('Wishlist', backref='book', lazy=True)
+
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.description}', '{self.price}', '{self.stock_level}')"
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True, nullable=False)
+    first_name = db.Column(db.String(15), nullable=False)
+    surname = db.Column(db.String(15), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    password = db.Column(db.String(60), nullable=False)
+    user_cart = db.relationship('Cart', backref='user', lazy=True)
+    user_wishlist = db.relationship('Wishlist', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'),nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+
+class Wishlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'),nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
