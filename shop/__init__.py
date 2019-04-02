ba320fb3d319005aef630c3cba85627ea2455d29
Joshua Marchant <marchantjr@cardiff.ac.uk>
2019-04-02 15:54:18 +0100

    database setup

--- shop/__init__.py
+++ shop/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '9e46baba727119686172c5d5ec8c94f31312a51baf966d6d'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c1846366:Monopoly@csmysql.cs.cf.ac.uk:3306/c1846366'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://group28.2018:jncPzvP4TVNF6gt@csmysql.cs.cf.ac.uk:3306/group28.2018'
 
 db = SQLAlchemy(app)
 login_manager = LoginManager()
